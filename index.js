77380282824ede4dda69009af8433c9a0683b724
Fredrick <freddyfred976@gmail.com>
2023-06-29 14:10:47 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const receivesAFunction = callback => {
-    callback();
-  };
-  
-  const returnsANamedFunction = () => {
-    return function namedFn() {
-      console.log("this is a named function");
-    };
-  };
-  
-  const returnsAnAnonymousFunction = () => {
-    return () => console.log("this is an anonymous function");
-  };
-  
+const receivesAFunction = (callback) => {
+  callback();
+};
+
+const returnsANamedFunction = () => {
+  return function namedFunction() {};
+};
+
+const returnsAnAnonymousFunction = () => {
+  return () => {};
+};
